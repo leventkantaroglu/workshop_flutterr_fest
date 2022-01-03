a5c9acc2e40e1d0580b503ed7e5817ea293d7e5f
Levent Kantaroğlu <leventkantaroglu@dusyeri.com.tr>
2022-01-03 18:16:02 +0300

    end of first day

---
+++ lib/detail_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  _DetailPageState createState() {
+    return _DetailPageState();
+  }
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'detail_page.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -26,8 +28,129 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  List<String>? students;
+
+  @override
+  void initState() {
+    students = [
+      "Levent",
+      "Hakan",
+      "Şule",
+      "Murat",
+      "Hande",
+    ];
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: (students == null)
+          ? const Center(
+              child: Text("İçerik bulunamadı"),
+            )
+          : SafeArea(
+              child: CustomScrollView(
+                slivers: [
+                  const SliverAppBar(
+                    expandedHeight: 200,
+                    pinned: true,
+                    title: Text("Ana sayfa"),
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        return listItemWidget(students![index]);
+                      },
+                      childCount: students!.length,
+                    ),
+                  ),
+                  SliverToBoxAdapter(
+                    child: Container(
+                      height: 100,
+                      color: Colors.green,
+                      child: const Center(
+                        child: Text("Kayıtlar başladı"),
+                      ),
+                    ),
+                  ),
+                  SliverGrid(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        return listItemWidget(students![index]);
+                      },
+                      childCount: students!.length,
+                    ),
+                  ),
+                  SliverGrid(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        return listItemWidget(students![index]);
+                      },
+                      childCount: students!.length,
+                    ),
+                  ),
+                  SliverGrid(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        return listItemWidget(students![index]);
+                      },
+                      childCount: students!.length,
+                    ),
+                  )
+                ],
+              ),
+            ),
+    );
+  }
+
+  Widget listWidget(List<String> students) {
+    return ListView.separated(
+      itemCount: students.length,
+      separatorBuilder: (context, index) {
+        return const SizedBox();
+      },
+      itemBuilder: (context, index) {
+        return listItemWidget(students[index]);
+      },
+    );
+  }
+
+  Widget listItemWidget(String student) {
+    return InkWell(
+      splashColor: Colors.green,
+      onTap: () {
+        goToDetailPage();
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(student),
+      ),
+    );
+  }
+
+  void goToDetailPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return const DetailPage();
+      }),
+    ).then((value) {
+      print("Dönüş yapıldı");
+    });
+    print("Düğmeye  basıldı");
   }
 }
